9e5b7da3f723ca695702cbb9de02d82b20b08623
5nik7 <nickflip86@gmail.com>
2023-11-04 08:21:21 -0700

    min

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,39 @@
 return {
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
   {
     "catppuccin/nvim",
     lazy = false,
     name = "catppuccin",
+    priority = 1000,
     opts = {
       flavour = "mocha",
       transparent_background = true,
       term_colors = true,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.50,
+      },
       integrations = {
         alpha = true,
         cmp = true,
@@ -14,7 +41,10 @@ return {
         illuminate = true,
         indent_blankline = { enabled = true },
         lsp_trouble = true,
-        mini = true,
+        mini = {
+          enabled = true,
+          indentscope_color = "overlay0",
+        },
         native_lsp = {
           enabled = true,
           virtual_text = {
@@ -42,6 +72,7 @@ return {
       },
     },
   },
+
   {
     "LazyVim/LazyVim",
     opts = {
