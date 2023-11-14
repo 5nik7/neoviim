c8342881d48e1a4a4a0b57664fc9da3ac359e68b
5nik7 <nickflip86@gmail.com>
2023-11-14 13:50:48 -0800

    v1.1

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,12 +5,12 @@ return {
     opts = {
       sources = { "filesystem", "buffers", "git_status", "document_symbols" },
       open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "Outline" },
+      close_if_last_window = true,
       filesystem = {
         follow_current_file = { enabled = true },
         visible = true,
         hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_hidden = false,
+        hide_gitignored = true,
       },
     },
   },
