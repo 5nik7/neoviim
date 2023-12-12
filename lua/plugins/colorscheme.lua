0b5dde93766378fe89ded76c7e4c579077917ad1
5nik7 <nickflip86@gmail.com>
2023-12-12 13:44:21 -0800

    1.7

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,9 +5,37 @@ return {
   },
 
   {
+    "oncomouse/lushwal.nvim",
+    cmd = { "LushwalCompile" },
+    dependencies = {
+      { "rktjmp/lush.nvim" },
+      { "rktjmp/shipwright.nvim" },
+    },
+  },
+
+  {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "moon" },
+    opts = {
+      style = "moon",
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "transparent", -- style for sidebars, see below
+        floats = "transparent", -- style for floating windows
+      },
+    },
+  },
+
+  {
+    "tanvirtin/monokai.nvim",
   },
 
   {
@@ -92,7 +120,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "pywal",
+      colorscheme = "tokyonight",
     },
   },
 }
